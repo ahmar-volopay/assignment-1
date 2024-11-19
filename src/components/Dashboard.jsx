fb08cc2e07e886e5c3aaf83741c96fd16c2a35b8
ahmar <ahmarcode@gmail.com>
2024-11-19 16:57:40 +0530

    add createSelectors for every attribute of data object

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,46 +1,51 @@
 import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchStocks } from '../store/actions/stockActions';
+import {
+    getTopGainersMemoized,
+    getTopLosersMemoized,
+    getLoadingState,
+    getErrorState,
+    getMetadataState
+} from '../store/selectors/stockSelectors';
 import Indicator from './Indicator';
 
 const DashBoard = () => {
     const dispatch = useDispatch();
-    const { loading, metadata, top_gainers, top_losers, error } = useSelector(
-        (state) => state.stocks
-    );
+
+    const topGainers = useSelector(getTopGainersMemoized);
+    const topLosers = useSelector(getTopLosersMemoized);
+    const loading = useSelector(getLoadingState);
+    const error = useSelector(getErrorState);
+    const metadata = useSelector(getMetadataState);
 
     useEffect(() => {
         dispatch(fetchStocks());
     }, [dispatch]);
 
+    const renderError = (errorMsg) => (
+        <div className="text-center text-red-500 font-semibold py-2">
+            Error: {errorMsg}
+        </div>
+    );
+
+    const renderLoading = (type) => (
+        <p>Loading {type}...</p>
+    );
+
     return (
         <div className="container mx-auto p-4">
             <h1 className="text-center font-bold text-4xl py-4">DashBoard</h1>
-
             <div className="text-center font-semibold text-2xl py-2">
-                {loading ? <p>Loading metadata...</p> : metadata}
+                {loading ? renderLoading('metadata') : metadata}
             </div>
-
-            {error && (
-                <div className="text-center text-red-500 font-semibold py-2">
-                    Error: {error}
-                </div>
-            )}
-
+            {error && renderError(error)}
             <div className="flex justify-center gap-4 py-4">
                 <div>
-                    {loading ? (
-                        <p>Loading top gainers...</p>
-                    ) : (
-                        <Indicator data={top_gainers} type="gainers" />
-                    )}
+                    {loading ? renderLoading('top gainers') : <Indicator data={topGainers} type="gainers" />}
                 </div>
                 <div>
-                    {loading ? (
-                        <p>Loading top losers...</p>
-                    ) : (
-                        <Indicator data={top_losers} type="losers" />
-                    )}
+                    {loading ? renderLoading('top losers') : <Indicator data={topLosers} type="losers" />}
                 </div>
             </div>
         </div>
---
+++ src/store/selectors/stockSelectors.js
@@ -0,0 +1,40 @@
+import { createSelector } from 'reselect';
+const getTopGainers = (state) => state.stocks.top_gainers;
+const getTopLosers = (state) => state.stocks.top_losers;
+const getLoading = (state) => state.stocks.loading;
+const getError = (state) => state.stocks.error;
+const getMetadata = (state) => state.stocks.metadata;
+
+export const getTopGainersMemoized = createSelector(
+  [getTopGainers], 
+  (topGainers) => topGainers
+);
+
+export const getTopLosersMemoized = createSelector(
+  [getTopLosers],
+  (topLosers) => topLosers
+);
+
+export const getLoadingState = createSelector(
+  [getLoading],
+  (loading) => loading
+);
+
+export const getErrorState = createSelector(
+  [getError],
+  (error) => error
+);
+
+export const getMetadataState = createSelector(
+  [getMetadata],
+  (metadata) => metadata
+);
+
+export const getStocksSummary = createSelector(
+  [getTopGainers, getTopLosers],
+  (topGainers, topLosers) => ({
+    totalStocks: topGainers.length + topLosers.length,
+    topGainersCount: topGainers.length,
+    topLosersCount: topLosers.length,
+  }) 
+);
